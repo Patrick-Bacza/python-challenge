deebc48f9f9e911495a699fbc0081d86b58d6d26
Patrick Bacza <Patrick.Bacza@outlook.com>
2023-01-16 20:42:55 -0500

    Updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,11 +3,12 @@
 import os
 import csv 
 
+ 
 month_year = []
 profit_loss = []
 
 
-
+# Write in csv file
 
 csvpath = os.path.join('PyBank' , 'Resources' , 'budget_data.csv')
 
@@ -17,18 +18,26 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
 
     print(csvreader)
+    
+#Grab Headers
 
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
+# Loop through file, append profit loss list with values changed to integers and run calculations
 
     for row in csvreader:
+        month_year.append(row[0])
         profit_loss.append(int(row[1]))
         total_months = len(profit_loss)
         total = sum(profit_loss)
         max_increase = max(profit_loss)
         max_decrease = min(profit_loss)
-    if row[1] == str(max_increase):
-        max_month = row[0]
+        max_increase_date = month_year[profit_loss.index(max_increase)]
+        max_decrease_date = month_year[profit_loss.index(max_decrease)]
+
+
+
+   
 
 
 print('Financial Analysis')
@@ -39,9 +48,9 @@ print(f'Total Months: {total_months}')
 
 print(f'Total: ${total}')
 
-print(f'Greatest Increase in Profits: {max_month} ({max_increase})')
+print(f'Greatest Increase in Profits: {max_increase_date} ({max_increase})')
 
-print(f'Greatest Decrease in Profits: {min_month} ({max_decrease})')
+print(f'Greatest Decrease in Profits: {max_decrease_date} ({max_decrease})')
 
 
     
